36a6082bfe41dbcd1303eed9afd53a5e037e03b3
Gankhulug <huluguu0202@gmail.com>
2023-01-08 20:48:38 +0800

    asd

--- myapp/src/components/footer.jsx
+++ myapp/src/components/footer.jsx
@@ -2,11 +2,9 @@ import Container from "react-bootstrap/Container";
 
 const Footer = () => {
   return (
-    // <div className="d-flex">
     <Container>
       <img src="/images/zurag4.svg" alt="" className="auto" />
     </Container>
-    // </div>
   );
 };
 
