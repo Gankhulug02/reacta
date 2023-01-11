08b8a692cc00274a4ed0ba49dc785b1d02e1f8b9
Gankhulug02 <huluguu0202@gmail.com>
2023-01-11 16:19:09 +0800

    asd

--- second-app/src/components/cardList/index.jsx
+++ second-app/src/components/cardList/index.jsx
@@ -3,11 +3,18 @@ import React from "react";
 import CardItem from "../cardItem/index";
 
 const Cardlist = ({ users }) => {
+  let empty = "";
+  if (users.length == 0) {
+    empty =
+      "https://www.meme-arsenal.com/memes/6db9949e601ea8d2e4db2e75fa775dc4.jpg";
+  }
+
   return (
     <div className="row gap-4">
       {users.map((user) => (
-        <CardItem user={user} />
+        <CardItem user={user} key={user.first} />
       ))}
+      <img src={empty} alt="" className="w-50 m-auto mt-5" />
     </div>
   );
 };
